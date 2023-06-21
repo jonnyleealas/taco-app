8927a838d1d50f5ddbdbcf6ad3b9453ed7fd1bc8
jonathon lee <jonnyleealas@gmail.com>
2023-06-21 12:43:23 -0500

    Added inputs and some styling to signOn

--- src/Components/sign-on/Sign-on.js
+++ src/Components/sign-on/Sign-on.js
@@ -1,7 +1,35 @@
+import './sign-on.css'
+
 const SignOn = () => {
     return (
-        <div>
-            sign on
+        <div className="sign-on-card">
+            <h1>
+                Create your account
+            </h1>
+            <div>
+                Or
+            </div>
+            <div>
+                <div>
+                    Name
+                </div>
+                <input type="text" placeholder=" Name"></input>
+            </div>
+            <div>
+                <div>
+                    Email
+                </div>
+                <input type='text' placeholder=" Email"></input>
+            </div>
+            <div>
+                <div>
+                    Password
+                </div>
+                <input type="text" placeholder=" Password"></input>
+            </div>
+            <div>
+                <button>Sign Up</button>
+            </div>
         </div>
     )
 }
