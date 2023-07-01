7295f62218fed1307dd503e9dfb0a219981b8286
jozue06 <jozue06@gmail.com>
2023-06-30 22:08:15 -0500

    server start huge commit

---
+++ backend/src/startup/router.ts
@@ -0,0 +1,10 @@
+import { Express, Request, Response } from 'express';
+
+const routerSetup = (app: Express) =>
+  app
+
+  .get('/', async (req: Request, res: Response) => {
+    res.send('Hello World!');
+  });
+
+export default routerSetup;
