10c605c6ebe1441fa9a896f422c445a86496468a
jonathon lee <jonnyleealas@gmail.com>
2023-07-03 18:05:15 -0500

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -11,7 +11,7 @@ const Profile = () => {
             <article>
             <h1>{user.name}</h1>
             <p>Access has been granted</p>
-            {/* {JSON.stringify(user)} */}
+          
             <LogOutButton />
         </article>
         )
