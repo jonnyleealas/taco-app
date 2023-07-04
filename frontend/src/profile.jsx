1a9b7a34388d0a2878d0a589134308f0c8cdd5b3
jozue06 <jozue06@gmail.com>
2023-07-04 15:13:38 -0500

    api and stuff

--- frontend/src/profile.jsx
+++ frontend/src/profile.jsx
@@ -4,7 +4,21 @@ import LogOutButton from "./log-out-button";
 
 function Profile() {
   const { user, isAuthenticated } = useAuth0();
-
+  // try to connect this to our backend.
+  fetch(':3001/api/v1/users/getOrCreateUserByEmail', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      firstName: user.given_name,
+      lastName: user.family_name,
+      email: user.email
+    }),
+  })
+}
+  // we should try to hit our backend at :3001/api/v1/users/getOrCreateUserByEmail/?email=`${user.email}`
   return (
 
     isAuthenticated && (
