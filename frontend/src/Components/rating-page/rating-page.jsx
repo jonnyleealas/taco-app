9fbea4b61c397e5601dadde8b81e67fe5b52f147
jonathon lee <jonnyleealas@gmail.com>
2023-07-16 20:59:28 -0500

    added styling to slider
    Added js

--- frontend/src/Components/rating-page/rating-page.jsx
+++ frontend/src/Components/rating-page/rating-page.jsx
@@ -2,18 +2,20 @@ import { useState, React } from "react";
 import "./rating-page.css";
 
 function RatingPage() {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(1);
+
+  const handleSlide = (e) => {
+    setRating(e.target.value);
+  };
 
   return (
     <div className="rating-page-container">
       <div>
         <h1>Mcdonalds</h1>
-      </div>
-      <div>
-        <input type="range" min="0" max="10" step=".1" value={rating} onChange={(e) => setRating(e.target.value)} />
-      </div>
-      <div>
         {rating}
+      </div>
+      <div>
+        <input className="slider" type="range" min="1" max="10" step=".1" value={rating} onChange={handleSlide} />
       </div>
       <div>
         Rating
