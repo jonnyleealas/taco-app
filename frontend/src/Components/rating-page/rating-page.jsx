d177581f3dc1c55eaac534f3fe52a688b6d24fb0
jonathon lee <jonnyleealas@gmail.com>
2023-07-16 20:47:38 -0500

    added range slider

--- frontend/src/Components/rating-page/rating-page.jsx
+++ frontend/src/Components/rating-page/rating-page.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { useState, React } from "react";
 import "./rating-page.css";
 
 function RatingPage() {
+  const [rating, setRating] = useState(0);
+
   return (
     <div className="rating-page-container">
       <div>
         <h1>Mcdonalds</h1>
+      </div>
+      <div>
+        <input type="range" min="0" max="10" step=".1" value={rating} onChange={(e) => setRating(e.target.value)} />
+      </div>
+      <div>
+        {rating}
       </div>
       <div>
         Rating
