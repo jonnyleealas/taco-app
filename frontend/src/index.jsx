858155de409e0bfb8f21c48d3df939e291e5453a
jonathon lee <jonnyleealas@gmail.com>
2023-07-22 20:48:09 -0500

    header left and right adjustments for logos and user icon

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import {Auth0Provider} from "@auth0/auth0-react";
-import App from "./app";
+import App from "./App";
 import "./index.css";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
