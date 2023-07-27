3970a3adfac3705f8558fde9d965e475294a8815
jonathon lee <jonnyleealas@gmail.com>
2023-07-27 09:27:45 -0500

    playing with css for drag n drop

--- frontend/src/Components/rating-page/drag-drop/Drag-drop.jsx
+++ frontend/src/Components/rating-page/drag-drop/Drag-drop.jsx
@@ -1,33 +1,33 @@
 import {React, useMemo, useCallback} from "react";
 import {useDropzone} from "react-dropzone";
 
-const baseStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "20px",
-  borderWidth: 2,
-  borderRadius: 5,
-  borderColor: "#eeeeee",
-  borderStyle: "dashed",
-  backgroundColor: "#fafafa",
-  color: "#bdbdbd",
-  outline: "none",
-  transition: "border .24s ease-in-out",
-};
+// const baseStyle = {
+//   flex: 1,
+//   display: "flex",
+//   flexDirection: "column",
+//   alignItems: "center",
+//   padding: "20px",
+//   borderWidth: 2,
+//   borderRadius: 5,
+//   borderColor: "#eeeeee",
+//   borderStyle: "dashed",
+//   backgroundColor: "#fafafa",
+//   color: "#bdbdbd",
+//   outline: "none",
+//   transition: "border .24s ease-in-out",
+// };
 
-const focusedStyle = {
-  borderColor: "#2196f3",
-};
+// const focusedStyle = {
+//   borderColor: "#2196f3",
+// };
 
-const acceptStyle = {
-  borderColor: "#00e676",
-};
+// const acceptStyle = {
+//   borderColor: "#00e676",
+// };
 
-const rejectStyle = {
-  borderColor: "#ff1744",
-};
+// const rejectStyle = {
+//   borderColor: "#ff1744",
+// };
 // eslint-disable-next-line valid-jsdoc
 /**
  *  The Modal component
@@ -59,26 +59,26 @@ function Drag(props) {
     open,
   } = useDropzone({onDrop, accept: "image/*", noClick: true, noKeyboard: true});
 
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isFocused ? focusedStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {}),
-  }), [
-    isFocused,
-    isDragAccept,
-    isDragReject,
-  ]);
+  // const style = useMemo(() => ({
+  //   ...baseStyle,
+  //   ...(isFocused ? focusedStyle : {}),
+  //   ...(isDragAccept ? acceptStyle : {}),
+  //   ...(isDragReject ? rejectStyle : {}),
+  // }), [
+  //   isFocused,
+  //   isDragAccept,
+  //   isDragReject,
+  // ]);
 
   return (
 
 
     <div className="container">
-      <div {...getRootProps({style})}>
+      <div {...getRootProps()}>
         <input {...getInputProps()} />
-        <p>Drag n drop some files here, or click to select files</p>
+        <p>Drag and drop photos here</p>
         <button type="button" onClick={open}>
-          Open File Dialog
+        Browse Files
         </button>
         <p {...getRootProps({
           onClick: props.onClose,
