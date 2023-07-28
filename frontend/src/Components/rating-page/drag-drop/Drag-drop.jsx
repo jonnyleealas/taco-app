b03d67f6258e35d06cc421173126a052d25b7c61
jonathon lee <jonnyleealas@gmail.com>
2023-07-28 10:33:56 -0500

    removed uncommented code

--- frontend/src/Components/rating-page/drag-drop/Drag-drop.jsx
+++ frontend/src/Components/rating-page/drag-drop/Drag-drop.jsx
@@ -2,33 +2,7 @@
 import {React, useMemo, useCallback} from "react";
 import {useDropzone} from "react-dropzone";
 
-// const baseStyle = {
-//   flex: 1,
-//   display: "flex",
-//   flexDirection: "column",
-//   alignItems: "center",
-//   padding: "20px",
-//   borderWidth: 2,
-//   borderRadius: 5,
-//   borderColor: "#eeeeee",
-//   borderStyle: "dashed",
-//   backgroundColor: "#fafafa",
-//   color: "#bdbdbd",
-//   outline: "none",
-//   transition: "border .24s ease-in-out",
-// };
 
-// const focusedStyle = {
-//   borderColor: "#2196f3",
-// };
-
-// const acceptStyle = {
-//   borderColor: "#00e676",
-// };
-
-// const rejectStyle = {
-//   borderColor: "#ff1744",
-// };
 // eslint-disable-next-line valid-jsdoc
 /**
  *  The Modal component
@@ -61,16 +35,6 @@ function Drag(props) {
     open,
   } = useDropzone({onDrop, accept: "image/*", noClick: true, noKeyboard: true});
 
-  // const style = useMemo(() => ({
-  //   ...baseStyle,
-  //   ...(isFocused ? focusedStyle : {}),
-  //   ...(isDragAccept ? acceptStyle : {}),
-  //   ...(isDragReject ? rejectStyle : {}),
-  // }), [
-  //   isFocused,
-  //   isDragAccept,
-  //   isDragReject,
-  // ]);
 
   return (
     <div className="overlay" onClick={props.onClose} role="presentation">
