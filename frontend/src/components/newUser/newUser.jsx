f912aa9e3edf977561c245d74ac8f7657a2e86c5
Jonathon Lee <jonnyleealas@gmail.com>
2024-08-10 20:54:07 -0500

    created card for new user

--- frontend/src/components/newUser/newUser.jsx
+++ frontend/src/components/newUser/newUser.jsx
@@ -2,31 +2,28 @@
 import "./newUser.css"
 function NewUser() {
     return (
-        <div className="newUserCard">
-            <h2>
-                New User
-            </h2>
-            <div>
-                <label>
-                    First Name
-                    <input type="text" placeholder="first name" />
-                </label>
-
-                <label>
-                    Last Name
-                    <input type="text" placeholder="Last Name" />
-                </label>
-                <label>
-                     Email
-                    <input type="text" placeholder="Email" />
-                </label>
-                <label>
-                    Favorite Color
-                    <input type="text" placeholder="Favorite Color" />
-                </label>
-                
-            </div>
+        
+        <form>
+            <div className="newUserCard">
+            <label>
+                First Name
+                <input type="text" placeholder="Fist Name" />
+            </label>
+            <label>
+                Last Name
+                <input type="text" placeholder="Last Name" />
+            </label>
+            <label>
+                Email
+                <input type="text" placeholder="Email" />
+            </label>
+            <label>
+                Favorite Color
+                <input type="text" placeholder="Favorite Color" />
+            </label>
+            <input type="submit" placeholder="Submit" />
         </div>
+        </form>
     )
 }
 
