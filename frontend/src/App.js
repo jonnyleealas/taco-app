2f6f50bbb7c6c891dc3d65c55d44521536ff6769
Jonathon Lee <jonnyleealas@gmail.com>
2024-08-13 01:46:07 -0500

    added show user starter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import './components/newUser/newUser'
 import NewUser from './components/newUser/newUser';
+import ShowUser from './components/showUser/showUser';
 
 
 function App() {
   return (
     <div className="App">
       
-        <NewUser />
+        {/* <NewUser /> */}
+        <ShowUser />
     
     </div>
   );
---
+++ frontend/src/components/showUser/showUser.jsx
@@ -0,0 +1,15 @@
+import {useState} from "react"
+import "./showUser.css"
+
+function ShowUser() {
+   
+
+    return (
+        
+       <div className="showUserCard">
+
+       </div>
+    )
+}
+
+export default ShowUser
