d806e9d3d722da6dd3c5b6136265dbacf188b2e9
Jonathon Lee <jonnyleealas@gmail.com>
2024-08-13 23:03:11 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import './components/newUser/newUser'
 // import NewUser from './components/newUser/newUser';
 import ShowUser from './components/showUser/showUser';
-// import "../node_modules/react-bootstrap/dist/react-bootstrap"
+
 
 
 
