9fb58b79fc31824d4b47c2f2592eca441f198663
Jonathon Lee <jonnyleealas@gmail.com>
2024-08-13 19:14:57 -0500

    played with colors in bootstrap

--- frontend/src/components/showUser/showUser.jsx
+++ frontend/src/components/showUser/showUser.jsx
@@ -1,25 +1,28 @@
 // import {useState} from "react"
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function ShowUser() {
    
 
     return (
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Card.Link href="https://google.com">Google</Card.Link>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>   
+        <>
+       
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      
+        </>
     )
 }
 
