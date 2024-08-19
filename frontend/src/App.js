a35798e4437dc520224b3652f04c741dcb0395c8
Jonathon Lee <jonnyleealas@gmail.com>
2024-08-18 22:49:08 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './components/newUser/newUser'
-// import NewUser from './components/newUser/newUser';
+import NewUser from './components/newUser/newUser';
 import ShowUsers from './components/ShowUsers/ShowUsers';
 // import ShowUser from './components/showUser/showUser';
 
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       
-        {/* <NewUser /> */}
-        <ShowUsers />
+        <NewUser />
+        {/* <ShowUsers /> */}
         {/* <ShowUser /> */}
     
     </div>
